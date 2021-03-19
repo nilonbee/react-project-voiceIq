b7c1e44ba78cb8c79b9967472339551bc1cd60a3
Nilan Athapaththu <78773065+nilonbee@users.noreply.github.com>
2021-03-19 14:12:13 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -38,35 +38,35 @@ const Login = () => {
         
             <container clasName="container-fluid">
 <div className="row align-self-center">
-    <div className="col-md-12 justify-self-center" >
+    <div className="col-lg-12 justify-self-center"  >
 
         <div className="title">
         <Title title="SIGN-UP" />
         </div>
 
-          <fieldset style={{ backgroundImage:"linear-gradient(to right,#ede,#FFFFFF)",padding:25}} className="fieldset fieldser-dark fieldset-rounded">  
-           <form className='form'  onSubmit={handleSubmit}>
+          <fieldset className="shadow-lg p-3 mb-5 bg-body rounded" style={{  backgroundImage:"linear-gradient(to right,#D4BBFE,#A3FEEF)",padding:25,borderRadius:28,textShadow:7}} >  
+           <form style={{paddingLeft:45,paddingRight:45,paddingTop:45}}className='form'  onSubmit={handleSubmit}>
             <div className="row">
-        <div className="col-md-12 justify-self-center">
+        <div className="col-md-12 justify-self-center" >
               <label className="form-label">FirstName</label>:
-              <input onChange={handleChange}    value={person.firstName} required  id="firstName" name="firstName" type="text" className="form-control" placeholder="type first name here"/>          </div>
+              <input  style={{textShadow:6}}  onChange={handleChange}    value={person.firstName} required  id="firstName" name="firstName" type="text" className="form-control" placeholder="type first name here"/>          </div>
         </div>
-        <div className="row">
+        <div style={{marginTop:12}} className="row">
         <div className="col-md-12 justify-self-center">
               <label className="form-label">LastName</label>:
               <input onChange={handleChange}  value={person.lastName} required id="lastName" name="lastName" type="text" className="form-control" placeholder="type last name here"/>          </div>
         </div>
-        <div className="row">
+        <div style={{marginTop:12}} className="row">
         <div className="col-md-12 justify-self-center">
               <label className="form-label">Email</label>:
               <input onChange={handleChange} value={person.email} required id="email"  name="email" type="Email" className="form-control" placeholder="type email here"/>          </div>
         </div>
-        <div className="row">
+        <div style={{marginTop:12}} className="row">
         <div className="col-md-12 justify-self-center">
-              <label className="form-label">Password</label>:
+              <label className="form-label">LastName</label>:
               <input   required   name="password" type="password" id="password" className="form-control" placeholder="type password here"/>          </div>
-        </div>
-        <div className="row">
+        </div >
+        <div style={{marginLeft:15,marginTop:35}} className="row">
         <div className="col-md-12 justify-self-center">
         <button className="btn btn-primary btn-sm btn-rounded m-3 " type="submit"> Sign-Up</button>       </div>
         </div>
@@ -101,10 +101,10 @@ const Login = () => {
 {people.map((person)=>{
                 const{firstName,lastName,email,id,} = person
                 if(person.firstName&&person.lastName&&person.email)
-                {return <Submit className="fieldset"><div key={id}>
-            <div style={{ backgroundImage:"linear-gradient(to right,#f67a36,#ed008c)",padding:25}}>
-                <h1> CONGRATULATIONS!!</h1>   
-                  <h3><u>You are already Signed-Up </u></h3>
+                {return <Submit ><div key={id}className="fieldset">
+            <div style={{ backgroundImage:"linear-gradient(to right,#f67a36,#ed008c)",padding:25,}}>
+                <h4> CONGRATULATIONS!!</h4>   
+                  <h5><u>You are already Signed-Up </u></h5>
                   <h6>FirstName :<br/>{firstName}</h6>
                   <h6>LastName:<br/>{lastName}</h6>
                   <h6>Email : <br/>{email}</h6>
@@ -117,16 +117,75 @@ const Login = () => {
               })
             }
            
-      
-        
         <Services/>
-        
-         </>
+              </>
 
      );
 }
  
 export default Login;
+/*
+import React, { useState } from 'react';
+
+const MoreControlledInputs = () => {
+
+   
+   const[person,setPerson] = useState({firstName:'',email:'',age:''})
+
+   const [people,setPeople] = useState([]) 
+
+const handleChange = (e)=>{
+    const name = e.target.name
+    const value = e.target.value
+    setPerson({...person,[name]:value})
+}
+   
+  const handleSubmit = (e)  =>{
+      e.preventDefault()
+      if(person.firstName && person.email&& person.age){
+          const newPerson = {...person}
+          setPeople([...people,newPerson])
+          setPerson({firstName:'',email:'',age:''})
+          
+      }
+  }
+   
+    return ( 
+    
+    <article>
+        <form className='form' onSubmit={handleSubmit}>
+        <div>
+            <label htmlFor='firstName'>FirstName</label>
+            <input value={person.firstName} name='firstName' id='firstName' onChange={handleChange} type='text'/>
+        </div>
+        <div>
+            <label htmlFor='email'>Email</label>
+            <input value={person.email} name='email' id='email' onChange={handleChange } type='text'/>
+        </div>
+        <div>
+            <label htmlFor='age'>age</label>
+            <input value={person.age} name='age' id='age' onChange={handleChange} type='text'/>
+        </div>
+        <button  type='submit' className='btn'>Save Items</button>
+
+        </form>
+
+        {people.map((person)=>{
+                const{firstName,email,id,age} = person
+                return<div key={id}>
+                    <h4>{firstName}</h4>
+                    <h4>{email}</h4>
+                    <h4>{age}</h4>
+                </div>
+            
+            })
+        }
+        
+    </article> );
+}
+ 
+export default MoreControlledInputs;
+*/
 /*
 import React, { useState } from 'react';
 
